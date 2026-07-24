1412e372a5270a6d223c0b6a3da32006663d03b5
SasaCBW <sara.wendling@escola.pr.gov.br>
2026-07-23 22:12:19 -0300

    Refactor photo upload logic and client loading

--- js/upload.js
+++ js/upload.js
@@ -1,4 +1,17 @@
-import { storage } from "../firebase.js";
+import { db, storage } from "../firebase.js";
+
+
+import {
+
+collection,
+getDocs
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 
 
 import {
@@ -14,46 +27,133 @@ from
 
 
 
-const botao =
-document.getElementById("enviar");
 
-
-
-botao.addEventListener("click", async()=>{
+const clienteSelect =
+document.getElementById("cliente");
 
 
 const arquivos =
-document.getElementById("arquivos").files;
+document.getElementById("fotos");
 
 
 
-if(arquivos.length === 0){
+const botao =
+document.getElementById("enviarFotos");
 
-alert("Selecione as fotos.");
 
-return;
+
+
+
+// Carregar clientes
+
+
+async function carregarClientes(){
+
+
+const dados = await getDocs(
+
+collection(db,"clientes")
+
+);
+
+
+
+clienteSelect.innerHTML="";
+
+
+
+dados.forEach((cliente)=>{
+
+
+const dadosCliente =
+cliente.data();
+
+
+
+clienteSelect.innerHTML += `
+
+
+<option value="${cliente.id}">
+
+${dadosCliente.nome}
+
+</option>
+
+
+`;
+
+
+
+});
+
 
 }
 
 
 
-for(let foto of arquivos){
+carregarClientes();
 
 
 
-const localFoto = 
-"clientes/" + foto.name;
+
+
+
+
+// Enviar fotos
+
+
+botao.addEventListener("click", async()=>{
+
+
+const clienteId =
+clienteSelect.value;
+
+
+
+const fotos =
+arquivos.files;
+
+
+
+if(!fotos.length){
+
+
+alert("Escolha as fotos.");
+
+return;
+
+
+}
+
+
+
+
+
+for(let foto of fotos){
+
+
+
+const caminho =
+
+"clientes/" + clienteId + "/" + foto.name;
+
 
 
 
 const referencia =
-ref(storage, localFoto);
+
+ref(storage,caminho);
+
+
 
 
 
 await uploadBytes(
+
 referencia,
+
 foto
+
 );
 
 
@@ -62,9 +162,11 @@ foto
 
 
 
+
 document.getElementById("resultado").innerHTML =
 
 "Fotos enviadas com sucesso!";
+
 
 
 });
